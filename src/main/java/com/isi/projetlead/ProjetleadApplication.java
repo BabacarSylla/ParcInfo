fc24d5ad1fa2140eacb1a2ec2ab91db63d8ab2f0
BabacarSylla <babisko9817@gmail.com>
2022-12-05 23:43:52 +0000

    initialisation projet

---
+++ src/main/java/com/isi/projetlead/ProjetleadApplication.java
@@ -0,0 +1,13 @@
+package com.isi.projetlead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetleadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetleadApplication.class, args);
+	}
+
+}
